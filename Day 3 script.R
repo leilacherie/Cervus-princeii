1b01d451d254b7703e1dd454bccbf55594fb5f65
Leila <dsouzaleila@yahoo.co.uk>
2020-01-15 12:00:34 +0000

    Added instructions to save and commit

--- Day 3 script.R
+++ Day 3 script.R
@@ -1,2 +1,9 @@
 # Github training
 
+# To add to Github, click tick box next to folder in Git tab
+# To commit, press commit button
+
+mydata <-read.csv("")
+
+# When you change something and press save an 'M' in a blue box appears = modified
+# Put tick next to script file, new button appears: Diff shows you the modifications made
