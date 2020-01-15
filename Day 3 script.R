26c1e80c29c8cd7bddf4c361e79427ec2acb7a0a
Leila <dsouzaleila@yahoo.co.uk>
2020-01-15 12:23:46 +0000

    Instructions about gitignore and what should be comitted

--- Day 3 script.R
+++ Day 3 script.R
@@ -7,3 +7,19 @@ mydata <-read.csv("")
 
 # When you change something and press save an 'M' in a blue box appears = modified
 # Put tick next to script file, new button appears: Diff shows you the modifications made
+# Then commit it
+# Then push your code to Github - top right arrow in box
+
+# remember your password for Github! 
+
+# gitignore
+# can tell git what to ignore (e.g. large files, PDFs, Word docs, xls. etc)
+# these files are too big and too big to commit
+# changes made can't be seen or highlighted
+
+# what you should and shouldn't commit
+# code mainly
+# should commit regularly in chunks rather than at the end of each day
+# small simple files only (not, see above)
+
+
